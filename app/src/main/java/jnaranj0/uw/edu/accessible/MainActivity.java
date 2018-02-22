876691556f18ca578fc292e36fa1699715cfbc17
Luis Naranjo <luisnaranjo733@gmail.com>
2018-02-21 22:17:19 -0800

    hit the restart button

--- app/src/main/java/jnaranj0/uw/edu/accessible/MainActivity.java
+++ app/src/main/java/jnaranj0/uw/edu/accessible/MainActivity.java
@@ -1,123 +1,30 @@
 package jnaranj0.uw.edu.accessible;
 
-import android.content.Context;
-import android.net.wifi.WifiManager;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.widget.Toast;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity implements HierarchyFragment.OnSSIDClickedListener, ActiveConnectionFragment.OnSSIDSavedListener {
-    public static final String TAG = "**ACC_MAIN";
-
-    public HierarchyFragment hierarchyFragment;
-    public ActiveConnectionFragment activeConnectionFragment;
-
-    WifiManager wifiManager;
+public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
-
-        if (savedInstanceState != null) {
-            // The Activity IS being re-created so we don't need to instantiate the Fragment or add it,
-            // but if we need a reference to it, we can use the tag we passed to .replace
-
-            activeConnectionFragment =  (ActiveConnectionFragment) getSupportFragmentManager()
-                    .findFragmentByTag(ActiveConnectionFragment.TAG);
-
-            hierarchyFragment = (HierarchyFragment) getSupportFragmentManager()
-                    .findFragmentByTag(HierarchyFragment.TAG);
-            hierarchyFragment.setRetainInstance(true);
-
-        } else {
-            // The Activity is NOT being re-created so we can instantiate a new Fragment
-            // and add it to the Activity
-
-            activeConnectionFragment = new ActiveConnectionFragment();
-            hierarchyFragment = new HierarchyFragment();
-
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.topPane, activeConnectionFragment, ActiveConnectionFragment.TAG)
-                    .replace(R.id.bottomPane, hierarchyFragment, HierarchyFragment.TAG)
-                    .commit();
-        }
+        this.setContentView(R.layout.main_activity);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.settings_menu, menu);
-        MenuItem settingsItem = menu.findItem(R.id.reconnect_wifi_option);
-        settingsItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                Toast.makeText(MainActivity.this, R.string.menu_option_forced_wifi_handoff, Toast.LENGTH_SHORT).show();
-                wifiManager.reassociate();
-                // Return true to consume this click and prevent others from executing.
-                return true;
-            }
-        });
-        return true;
-    }
-
-
-    /*
-
-    This method is used for transitioning to the DetailSSIDFragment
-    */
-
-    @Override
-    public void onSSIDClicked(SSID ssid) {
-        FragmentManager manager = getSupportFragmentManager();
-        FragmentTransaction ft = manager.beginTransaction();
-
-        DetailSSIDFragment detailSSIDFragment = (DetailSSIDFragment) getSupportFragmentManager()
-                .findFragmentByTag(DetailSSIDFragment.TAG);
-
-        if (detailSSIDFragment == null) {
-            detailSSIDFragment = new DetailSSIDFragment();
-            Bundle bundle = new Bundle();
-            bundle.putLong(DetailSSIDFragment.BUNDLE_ARG_SSID_PK, ssid.getId());
-            detailSSIDFragment.setArguments(bundle);
-
-            ft.replace(R.id.bottomPane, detailSSIDFragment, DetailSSIDFragment.TAG);
-            ft.addToBackStack(null);
-            ft.commit();
-        }
-
-
-    }
-
-    // this gets called when a new base station is remembered
-    // guaranteed that ssid and bssid are not null
-    @Override
-    public void onSwitchToDetail(SSID ssid, BSSID bssid) {
-        hierarchyFragment.ssidAdapter.add(ssid);
-
-        // only present if we are currently in the detail view
-        DetailSSIDFragment detailSSIDFragment = (DetailSSIDFragment) getSupportFragmentManager()
-                .findFragmentByTag(DetailSSIDFragment.TAG);
-        if (detailSSIDFragment != null) {
-            detailSSIDFragment.bssidAdapter.add(bssid);
-        }
-        onSSIDClicked(ssid);
+    protected void onStart() {
+        super.onStart();
     }
 
     @Override
-    public void onBackPressed() {
-        if (getSupportFragmentManager().getBackStackEntryCount() > 0 ){
-            getSupportFragmentManager().popBackStack();
-        } else {
-            super.onBackPressed();
-        }
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
